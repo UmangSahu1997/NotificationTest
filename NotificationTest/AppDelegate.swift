f0fe5f2f397307b86deeb090181fb748ffce74af
Umang <sahuumang5@gmail.com>
2024-05-24 15:42:47 +0530

    Initial Commit

---
+++ NotificationTest/AppDelegate.swift
@@ -0,0 +1,72 @@
+//
+//  AppDelegate.swift
+//  NotificationTest
+//
+//  Created by Umang Sahu on 16/05/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        if launchOptions != nil {
+            XX.kString = "Success"
+        }
+        let notifications = UNUserNotificationCenter.current()
+        notifications.requestAuthorization(options: [.alert,.badge,.sound]) { success, err in
+            if success {
+                notifications.delegate = self
+                print("success")
+            }else {
+                print("some error")
+            }
+        }
+        application.registerForRemoteNotifications()
+        
+        if let notificationse = launchOptions?[.remoteNotification] as? [String: AnyObject] {
+            XX.kClick = true
+            //handleNotification(notification)
+        }
+        return true
+    }
+
+    func handleNotification(_ userInfo: [AnyHashable: Any]) {
+           // Handle the notification here
+           print("Received notification: \(userInfo)")
+       }
+    
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.sound, .badge, .alert])
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        //XX.kClick = true
+        completionHandler()
+    }
+}
+
+
+class XX {
+    static var kClick = false
+    static var kString = ""
+}
---
+++ NotificationTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NotificationTest
+//
+//  Created by Umang Sahu on 16/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var hasView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if XX.kClick {
+            hasView.backgroundColor = .red
+        }
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
