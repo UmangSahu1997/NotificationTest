8baa8af2745167505ea91b73ac90ebe93d6ed4ef
Umang <sahuumang5@gmail.com>
2024-05-24 16:09:42 +0530

    some work

--- NotificationTest/AppDelegate.swift
+++ NotificationTest/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         if launchOptions != nil {
             XX.kString = "Success"
+            print("lol")
         }
         let notifications = UNUserNotificationCenter.current()
         notifications.requestAuthorization(options: [.alert,.badge,.sound]) { success, err in
